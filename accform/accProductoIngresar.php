17942de67e26ee69038defaab5d4d9c115d79e88
cetecom <cetecom@LC08L1310>
2016-05-23 21:52:00 -0300

    adelanto 23-05-2016

---
+++ accform/accProductoIngresar.php
@@ -0,0 +1,14 @@
+<?php
+include("../librerias.php");
+$oPro=new Producto();
+$oAux=new Producto();
+$oAux->Nombre()=$_POST["nombre"];
+$oAux->TotalUSD()=$_POST["total"];
+$oAux->Ano()=$_POST["ano"];
+
+$oPro->agrega($oAux->Nombre(),$oAux->TotalUSD(),$oAux->Ano());
+?>
+
+<script>
+	document.location.href="<?=PATHURL?>exportaciones_add.php";
+</script>
